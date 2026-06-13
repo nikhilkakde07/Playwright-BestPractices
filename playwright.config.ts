af6b5d58a8047a907e6011c058ab537eb3cda09c
Nikhil <nikhilkakde967@gmail.com>
2026-06-14 01:27:56 +0530

    YML file select browser dropdown

--- playwright.config.ts
+++ playwright.config.ts
@@ -73,29 +73,29 @@ export default defineConfig({
           ignoreDefaultArgs: ['--window-size=1280,720']
         }
       }
-    }
+    },
 
-    // Uncomment below when needed
 
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: {
-    //     channel: 'msedge'
-    //   }
-    // },
 
-    // {
-    //   name: 'Firefox',
-    //   use: {
-    //     ...devices['Desktop Firefox']
-    //   }
-    // },
+     {
+      name: 'Microsoft Edge',
+       use: {
+        channel: 'msedge'
+       }
+     },
 
-    // {
-    //   name: 'WebKit',
-    //   use: {
-    //     ...devices['Desktop Safari']
-    //   }
-    // }
+     {
+       name: 'Firefox',
+       use: {
+         ...devices['Desktop Firefox']
+       }
+     },
+
+     {
+       name: 'WebKit',
+       use: {
+         ...devices['Desktop Safari']
+       }
+     }
   ]
 });
