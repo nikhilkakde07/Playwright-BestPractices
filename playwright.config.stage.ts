690b6cf8d8b70650bb6f9fdefd94326be8125d93
Nikhil <nikhilkakde967@gmail.com>
2026-06-14 14:51:47 +0530

    lint files updates

--- playwright.config.stage.ts
+++ playwright.config.stage.ts
@@ -59,7 +59,7 @@ export default defineConfig({
     project: 'Playwright Best Practices Framework'
   },
 
-  projects: [
+   projects: [
     {
       name: 'Google Chrome',
 
@@ -73,29 +73,29 @@ export default defineConfig({
           ignoreDefaultArgs: ['--window-size=1280,720']
         }
       }
-    }
+    },
 
-    // Uncomment below when needed
+   
 
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: {
-    //     channel: 'msedge'
-    //   }
-    // },
+     {
+       name: 'Microsoft Edge',
+       use: {
+         channel: 'msedge'
+       }
+     },
 
-    // {
-    //   name: 'Firefox',
-    //   use: {
-    //     ...devices['Desktop Firefox']
-    //   }
-    // },
+     {
+       name: 'Firefox',
+       use: {
+         ...devices['Desktop Firefox']
+       }
+     },
 
-    // {
-    //   name: 'WebKit',
-    //   use: {
-    //     ...devices['Desktop Safari']
-    //   }
-    // }
+     {
+       name: 'WebKit',
+       use: {
+         ...devices['Desktop Safari']
+       }
+     }
   ]
 });
